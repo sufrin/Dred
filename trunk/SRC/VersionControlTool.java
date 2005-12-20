d43937c6988208791773391c89f69e40806f3c1b
sufrin <sufrin@524a4809-c2f3-0310-afd8-da361d7478c6>
2005-12-20 22:17:41 +0000

    git-svn-id: svn+ssh://linux.cs.ox.ac.uk/users/sufrin/SVN/Dred@136 524a4809-c2f3-0310-afd8-da361d7478c6

--- trunk/SRC/VersionControlTool.java
+++ trunk/SRC/VersionControlTool.java
@@ -15,8 +15,8 @@ import java.io.File;
         Tool that puts up a Latex Toolbar
 */
 public class VersionControlTool extends RunTool
-{ public VersionControlTool() { super("SVN/CVS"); }
-  public VersionControlTool(Preferences prefs) { super("SVN/CVS"); this.prefs = prefs; }
+{ public VersionControlTool() { super("SVN/CVS/RCS"); }
+  public VersionControlTool(Preferences prefs) { super("SVN/CVS/RCS"); this.prefs = prefs; }
   protected Preferences prefs;
 
   @Override
@@ -25,7 +25,8 @@ public class VersionControlTool extends RunTool
     return new VersionControlToolBar(session);
   }
 
-
+  static enum VC { SVN, CVS, RCS }
+  
   class VersionControlToolBar extends TextLine
   {
     /**
@@ -35,7 +36,7 @@ public class VersionControlTool extends RunTool
     
     /** The version control system in use
     */
-    String vcsName = "svn";
+    VC vcsName = VC.SVN;
   
     public VersionControlToolBar(EditorFrame session)
     {
@@ -49,26 +50,34 @@ public class VersionControlTool extends RunTool
       bar.setInputMap(JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT, m);
       bar.setInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW, m);
   
-      JMenuItem commit = but(new Act("Commit", "Commit using svn or cvs or rcs")
+      JMenuItem commit = but(new Act("Commit", "Commit this file using the selected version control system")
       {
         public void run()
         {
           doCheckin(getText().trim());
         }
       });
+      JMenuItem diff = but(new Act("Diff", "Get the differences between this file and a committed version")
+      {
+        public void run()
+        {
+          doDiff(getText().trim());
+        }
+      });
   
-      JMenu menu = new JMenu("SVN");
-      RadioItem.Group<String> vcs = new RadioItem.Group<String> ("VersionController", vcsName, "Set the version control system")
+      JMenu menu = new JMenu("VC:");
+      RadioItem.Group<VC> vcs = new RadioItem.Group<VC> ("VersionController", vcsName, "Set the version control system")
       { { run(); }
         public void run() { vcsName = value; }
       };
       menu.setToolTipText("Checkin this file");
       menu.add(commit);
+      menu.add(diff);
       menu.addSeparator();
-      menu.add(new RadioItem<String>(vcs, "CVS", "cvs"));
-      menu.add(new RadioItem<String>(vcs, "RCS", "rcs"));
-      menu.add(new RadioItem<String>(vcs, "SVN", "svn"));
-      menu.add(new RadioItem<String>(vcs, "?",   "?"));
+      menu.add(new RadioItem<VC>(vcs, "CVS", VC.CVS));
+      menu.add(new RadioItem<VC>(vcs, "RCS", VC.RCS));
+      menu.add(new RadioItem<VC>(vcs, "SVN", VC.SVN));
+      menu.add(new RadioItem<VC>(vcs, "?",   null));
       bar.add(menu);
       setLabel(bar);
       setBorder(BorderFactory.createBevelBorder(BevelBorder.RAISED));
@@ -99,15 +108,37 @@ public class VersionControlTool extends RunTool
       this.session.doSave();
       String fileName = session.doc.getFileName().getAbsolutePath();
       message = message.replaceAll("(['\\\\\"])", "\\\\$1");
-      if (message.equals("") && vcsName.equals("rcs")) message="%%";
-      String cmd = String.format( vcsName.equals("cvs") ? "cvs ci -m '%s' %s" : 
-                                  vcsName.equals("svn") ? "svn ci -m '%s' %s" :
-                                  vcsName.equals("rcs") ? "ci -l -m'%s' %s"   : "echo version control -m '%s' %s", message, fileName);
-      this.session.startProcess(cmd, "", reloader, null);
+      String cmd = null;
+      switch (vcsName)
+      { case CVS: cmd = String.format("cvs ci -m '%s' %s", message, fileName); message=""; break;
+        case SVN: cmd = String.format("svn ci -m '%s' %s", message, fileName); message=""; break;
+        case RCS: cmd = String.format("ci -l %s", fileName); break;
+        default:  cmd = String.format("echo commit -m '%s' %s", message, fileName); message=""; break;
+      }      
+      this.session.startProcess(cmd, message, reloader, null);
+    } 
+     
+    /**
+     * Diff the current document.
+     */
+    public void doDiff(String revision)
+    {
+      this.session.doSave();
+      String fileName = session.doc.getFileName().getAbsolutePath();
+      String cmd = null;
+      if (!revision.equals("")) revision = (vcsName==VC.RCS?"-r":"-r ")+revision;
+      switch (vcsName)
+      { case CVS: cmd = String.format("cvs diff %s %s", revision, fileName); break;
+        case SVN: cmd = String.format("svn diff %s %s", revision, fileName); break;
+        case RCS: cmd = String.format("rcsdiff %s %s", revision, fileName); break;
+        default:  cmd = String.format("echo diff %s %s", revision, fileName); break;
+      }      
+      this.session.startProcess(cmd, "");
     }  
   }
 
 }
+
 
 
 
