29d2f407972a8f5595191d676e71f7ec9a82ab5a
sufrin <sufrin@524a4809-c2f3-0310-afd8-da361d7478c6>
2006-04-24 17:06:10 +0000

    REVISION.java is now synthesised by ant
    git-svn-id: svn+ssh://linux.cs.ox.ac.uk/users/sufrin/SVN/Dred@162 524a4809-c2f3-0310-afd8-da361d7478c6

---
+++ trunk/SRC/REVISION.java
@@ -0,0 +1,6 @@
+package org.sufrin.dred;
+class REVISION
+{ 
+  public static String number = "161";
+  public static String date   = "Mon Apr 24 18:04:51 BST 2006";
+}
--- trunk/SRC/revision.sh
+++ trunk/SRC/revision.sh
@@ -3,10 +3,23 @@
 #       If svn exists and the current directory is under svn control then 
 #       output the latest svn revision number
 #
+function revision() {
 if which svn > /dev/null 2> /dev/null && svn info 1>/dev/null 2>/dev/null
 then
    svn info -R | grep Revision | sort | tail -1 | sed -e 's/Revision: //'
 else 
-   echo 99999
+   echo "(no svn revision)"
 fi
+}
+
+REVISION=`revision`
+DATE=`date`
+cat > BUILD/org/sufrin/dred/REVISION.java <<END
+package org.sufrin.dred;
+class REVISION
+{ 
+  public static String number = "$REVISION";
+  public static String date   = "$DATE";
+}
+END
 
