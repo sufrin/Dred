2c9dc56cc4650550b170def89bc4256f1c4fb7a1
sufrin <sufrin@524a4809-c2f3-0310-afd8-da361d7478c6>
2006-04-26 22:36:22 +0000

    DMG distribution for apple version
    git-svn-id: svn+ssh://linux.cs.ox.ac.uk/users/sufrin/SVN/Dred@178 524a4809-c2f3-0310-afd8-da361d7478c6

--- trunk/SRC/REVISION.java
+++ trunk/SRC/REVISION.java
@@ -1,6 +1,6 @@
 package org.sufrin.dred;
 class REVISION
 { 
-  public static String number = "176";
-  public static String date   = "Wed Apr 26 12:21:31 BST 2006";
+  public static String number = "177";
+  public static String date   = "Wed Apr 26 23:35:44 BST 2006";
 }
